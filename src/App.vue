c0f60c568684af912fca008d072e3a2dd968e3c1
April <hesiyuetian@gmail.com>
2021-04-06 17:24:43 +0800

    updata

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,7 @@
         }
 
         handleNewAccounts(acc) {
+            console.error('acc----->',acc)
             if (!acc.length) {
                 Storage.removeItem('account');
                 return;
@@ -61,6 +62,7 @@
         }
 
         handleChainChanged(chainId) {
+            console.error('chainId----->',chainId)
             if (!chainId) return;
 
             if (chainId != this.ChainId && this.ChainId) {
